3342f6bba65f7a51c283b2b937d062404639ffbe
asa1984 <satoasa9913@gmail.com>
2026-05-24 17:44:31 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        let
+          sharedPackages = with pkgs; [
+            nodejs-slim
+            corepack
+          ];
+        in
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              packages = sharedPackages;
+            };
+            ci = pkgs.mkShell {
+              packages = sharedPackages;
+            };
+          };
+          packages.ci = pkgs.buildEnv {
+            name = "asa1984-configs-ci";
+            paths = sharedPackages;
+          };
+        };
+    };
+}
